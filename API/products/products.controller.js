7466ff55494dc52663bff990feb2a02f24d9fe59
Remon Isaac Gamil <48691269+RemonIsaac@users.noreply.github.com>
2021-09-10 15:56:40 +0200

    implemented products controllers

--- API/products/products.controller.js
+++ API/products/products.controller.js
@@ -1,6 +1,9 @@
 const Exception = require("../../exceptions/Exception");
 const Product = require("./products.model");
-const getProducts = (req, res, next) => {
+const router = require("./products.routes");
+
+//getting all
+const getProducts = async (req, res, next) => {
   try {
     const products = await Product.find();
     res.send(products);
@@ -9,6 +12,92 @@ const getProducts = (req, res, next) => {
   }
 };
 
+const getProducts = ('/:id', async (req, res, next) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (product == null) {
+      next(new Exception(error.message, 404))
+    }
+    res.send(product);
+  } catch (error) {
+    next(new Exception(error.message, 500));
+  }
+});
+
+//getting one 
+const getProducts = ('/:id', async (req, res, next) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (product == null) {
+      next(new Exception(error.message, 404))
+    }
+    res.send(product);
+  } catch (error) {
+    next(new Exception(error.message, 500));
+  }
+});
+
+//posting product
+const postProduct = async (req, res, next) =>{
+  const product = new Product(req.body)
+
+  try{
+    await product.save()
+  } catch (error) {
+    next(new Exception("couldn't post product", 401))
+  }
+}
+
+//editing product
+const editProduct =('/:id', async(req, res, next) => {
+  try {
+    const product = await Product.findById(req.params.id)
+    if(product == null){
+      next(new Exception(error.message, 404))
+    }
+    if(req.body.title != null){
+      res.product.title = req.body.title
+    }
+
+    if(req.body.price != null){
+      res.product.price = req.body.price
+    }
+
+    if(req.body.description != null){
+      res.product.description = req.body.description
+    }
+
+    if(req.body.img != null){
+      res.product.img = req.body.img
+    }
+
+    if(req.body.quantity != null){
+      res.product.quantity = req.body.quantity
+    }
+    const updatedProduct = await res.product.save()
+    res.send(updatedProduct)
+
+  } catch (error) {
+    next(new Exception(error.message, 500))
+  }
+})
+
+//deleting product
+const deleteProduct = ('/:id', async(req, res, next) => {
+  try {
+    const product = await Product.findById(req.params.id)
+    product.remove()
+    res.send({
+      message: "Product deleted successfully."
+  });
+  } catch (error) {
+    next(new Exception(error.message, 400))
+  }
+})
+
 module.exports = {
   getProducts,
+  postProduct,
+  editProduct,
+  deleteProduct
 };

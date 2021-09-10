4619f8c9e5827efab15e7534e91a72a22847ab91
ziad1299 <34491178+ziad1299@users.noreply.github.com>
2021-09-10 21:33:06 +0200

    fixed implementation of product router

--- API/products/products.routes.js
+++ API/products/products.routes.js
@@ -3,7 +3,7 @@ const productsController = require('./products.controller');
 const router = Router();
 
 router.get('/',productsController.getProducts);
-router.get('/:id',productsController.getProducts);
+router.get('/:id',productsController.getProduct);
 router.post('/',productsController.postProduct);
 router.patch('/:id',productsController.editProduct);
 router.delete('/:id',productsController.deleteProduct);

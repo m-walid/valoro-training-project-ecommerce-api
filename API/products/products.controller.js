fe87076455bc087b021f976c14dffa53e5ab48fd
m-walid <49622775+m-walid@users.noreply.github.com>
2021-09-22 00:38:25 +0200

    fix: get product typo

--- API/products/products.controller.js
+++ API/products/products.controller.js
@@ -24,7 +24,7 @@ const getProduct = async (req, res, next) => {
     }else{
 
       res.send({
-        message: "Product fetched successfully.", products
+        message: "Product fetched successfully.", product
     })
     }
   } catch (error) {
